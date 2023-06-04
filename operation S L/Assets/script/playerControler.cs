b36b0392d92c378c21a980e6ab933498a7bd7647
MAdPlayer002 <56556509+Albert-Alfred@users.noreply.github.com>
2023-06-04 19:31:04 +0300

    player control

--- operation S L/Assets/script/playerControler.cs
+++ operation S L/Assets/script/playerControler.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class playerControler : MonoBehaviour
 {
+    private float speed = 10;
+    private float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,8 @@ public class playerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // horizontal movement
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.back * Time.deltaTime * speed * horizontalInput);
     }
 }
